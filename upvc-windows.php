fe7b32d0e8c86d325171b1b2820867d920390f7a
madhualupana9 <madhualupana7@gmail.com>
2026-04-21 11:04:00 +0530

    First commit

---
+++ upvc-windows.php
@@ -0,0 +1,141 @@
+<!DOCTYPE html>
+<html dir="ltr" lang="en">
+  <head>
+    <title>Urban Pro - uPVC Windows</title>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1"> 
+    <meta name="description" content="" />
+    <meta name="keywords" content="" />
+
+    <?php $page = 'one';  include "includes/header.php";?>
+    
+  </head>
+  <body class="counter-scroll header_sticky_style2">
+   
+    <?php include "includes/menu.php";?> 
+
+<div class="page-title page-title-inner padding-bottom9">
+            <div class="overlay-page-tile"></div>
+            <div class="page-title-content">
+                <div class="container">
+                    <div class="blog-title color-white">
+                        <h3>uPVC Windows</h3>
+                    </div>
+                </div>
+            </div>
+        </div><!-- Page-title -->
+
+
+           <!-- About Us  -->
+        <section class="about2 bg-color-style2 about-style">
+            <div class="container">
+                <div class="row">
+                    <div class="themesflat-spacer clearfix" data-desktop="60" data-mobile="60" data-smobile="60" ></div>
+                        
+                        <div class="col-sm-12 clearfix no-padding-right text-center">
+                        <div class="content-about ">
+                        <div class="title-section wow fadeInDown" style="margin-bottom: 20px;">
+                            <!-- <div class="sub-title">ABOUT US</div> -->
+                            <div class="flat-title margin5-8">
+                                UrbanPro uPVC Windows
+                            </div>
+                        </div>
+                        <p><b style="color: #4db848;font-size: 18px;">The elegant connection between inside and outside</b></p>
+                        <p style="margin-bottom: 10px;">Your window to a wider, more comfortable home is here. With a wide range of technically superior <b>uPVC windows manufactured</b> with the expertise of German Engineering, your home will be equipped with the perfect combination of comfortable living, high security, and the look of pure elegance.</p>
+                        <p>Choose from our range of uPVC windows, we have different styles to match up to your vision of a perfect home. With our wide range of options and superior German Engineering, we at UrbanPro believe that we are one of the <b>best uPVC Windows manufacturers</b> out there.</p>
+                        </div>
+                        </div>
+                </div>
+
+                <div class="row al-bx">
+                    <div class="col-md-6 offset-lg-1">
+                        <h3>uPVC Sliding Window</h3>
+                        <p>Sliding windows are quickly turning into a quintessential part of modern households because of their practicality and ease of use. Add to this the minimal style quotient of these windows and it’s fairly easy to understand why these windows are in such high demand. With our state-of-the-art sliding windows on offer, UrbanPro is one of the top uPVC sliding windows dealers in India.</p>
+                        <ul class="ul-list">
+                            <li><span>➤</span> Glazing thickness up to 32 mm</li>
+                            <li><span>➤</span> Extension profiles for multi-track guide rail available</li>
+                            <li><span>➤</span> Co-extruded gaskets</li>
+                            <li><span>➤</span> Excellent structural values allow for large elements</li>
+                            <li><span>➤</span> Aluminium threshold with integrated drain function</li>
+                            <li><span>➤</span> Easy installation</li>
+                        </ul>
+                    </div>
+                     <div class="col-md-5">
+                        <div class="image-hover about-image">
+                            <div class="image">
+                              <img src="assets/images/products/upvw1.png" class="img-responsive">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="row all-bx">
+                    <div class="col-md-5">
+                        <div class="image-hover about-image">
+                            <div class="image">
+                               <img src="assets/images/products/upvw2.png" class="img-responsive">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-6 offset-lg-1">
+                        <h3>uPVC Tilt and Turn Window</h3>
+                        <p>Tilt and turn windows are preferred by many homeowners because of their flexibility in terms of space requirements. Moreover, tilt and turn windows open inwards, adding an extra layer of security to them. These windows are also marginally easier to maintain because they open inwards, allowing you access to both the inner and outer portions of the window. At UrbanPro, we are working towards establishing ourselves among the <b>best uPVC tilt and turn window manufacturers.</b></p>
+                        <ul class="ul-list">
+                            <li><span>➤</span> Delicate profile design</li>
+                            <li><span>➤</span> Exchangeable gaskets</li>
+                            <li><span>➤</span> Steel reinforcement can be integrated into both frame and sash to ensure excellent structural values</li>
+                        </ul>
+                    </div>
+                </div>
+
+                <div class="row al-bx">
+                    <div class="col-md-6 offset-lg-1">
+                        <h3>uPVC Casement Window</h3>
+                        <p>Casement windows are extremely versatile and come with various possible window combinations. These windows also provide a great amount of ventilation and are quickly becoming one of the more prominent window options out there for personal and professional properties alike. These windows come with an integrated PVC interlock for seamless sealing. With our wide repository of options and colors to choose from, UrbanPro looks to be a premier <b>uPVC casement windows manufacturer.</b></p>
+                        <ul class="ul-list">
+                            <li><span>➤</span> Glazing thickness up to 18 mm</li>
+                            <li><span>➤</span> Unique sliding window with fixed and movable elements</li>
+                            <li><span>➤</span> Exchangeable gaskets</li>
+                            <li><span>➤</span> Wide range of accessories due to standardised 60 mm frame system</li>
+                            <li><span>➤</span> Integrated PVC interlock for perfect sealing</li>
+                            <li><span>➤</span> Fanlights and different window combinations are available</li>
+                        </ul>
+                    </div>
+                     <div class="col-md-5">
+                        <div class="image-hover about-image">
+                            <div class="image">
+                              <img src="assets/images/products/upvw3.png" class="img-responsive">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="row all-bx">
+                    <div class="col-md-5">
+                        <div class="image-hover about-image">
+                            <div class="image">
+                              <img src="assets/images/products/upvw4.png" class="img-responsive">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-6 offset-lg-1">
+                        <h3>uPVC Top Hung Window</h3>
+                        <p>UrbanPro’s top-hung windows stand out because of their user-friendly design and energy efficiency. These windows are robust and can withstand all kinds of extreme weather conditions. With our top-hung windows, you can rest assured that any undesirable element stays out. With UrbanPro, you get the most reliable <b>uPVC Top Hung Windows in Hyderabad.</b></p>
+                        <ul class="ul-list">
+                            <li><span>➤</span> Cost effective</li>
+                            <li><span>➤</span> Easy and Low-Cost Maintenance</li>
+                            <li><span>➤</span> Made from high-grade AIS uPVC</li>
+                            <li><span>➤</span> Smooth & Noiseless Operation</li>
+                        </ul>
+                    </div>
+                </div>
+                
+                <div class="themesflat-spacer clearfix" data-desktop="60" data-mobile="60" data-smobile="50" ></div>
+            </div>
+        </section>
+
+
+       
+
+
+    <?php include "includes/footer.php";?>
